d31c2d152a92a38a5f6b4fed0344279a5d0e4e8c
Maja Vuevska <maja.vuevska@gmail.com>
2022-03-10 21:00:45 +0100

    Add comment

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,6 +6,7 @@ public class MainClass {
         System.out.println("vto branch");
     }
 
+    // counts even numbers in an integer list
     public static int countEven(List<Integer> list) {
         return (int) list.stream().filter(i -> i % 2 == 0).count();
     }
