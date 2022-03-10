051d3b6fbc8838f7f8e05d1c66fb48a21bb8545f
Maja Vuevska <maja.vuevska@gmail.com>
2022-03-10 20:52:33 +0100

    Add method countEven

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,12 @@
+import java.util.List;
+
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Master branch");
         System.out.println("vto branch");
+    }
+
+    public static int countEven(List<Integer> list) {
+        return (int) list.stream().filter(i -> i % 2 == 0).count();
     }
 }
