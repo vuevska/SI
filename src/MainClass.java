5884000e4a7fbf7a64d5bf80c58009350e86ddf0
Maja Vuevska <maja.vuevska@gmail.com>
2022-03-21 16:35:43 +0100

    Add method to generate N random numbers

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,13 +1,22 @@
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class MainClass {
     public static void main(String[] args) {
         System.out.println("Master branch");
         System.out.println("vto branch");
+
+        System.out.println(generateRandomNNumberLessThanM(10, 100));
     }
 
     // counts even numbers in an integer list
     public static int countEven(List<Integer> list) {
         return (int) list.stream().filter(i -> i % 2 == 0).count();
+    }
+
+    public static List<Integer> generateRandomNNumberLessThanM(int n, int m) {
+        Random random = new Random();
+        return random.ints(n, 0, m).boxed().collect(Collectors.toList());
     }
 }
